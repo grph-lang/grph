41e0fd92f48deb43c3f2cb163981548af1eb958d
Snowy <3mille.prenom.nom@gmail.com>
2021-09-24 00:58:30 +0200

    Separate docgen tests, add method doc completeness check

--- Tests/GeneratorTests/DocGenTests.swift
+++ Tests/GeneratorTests/DocGenTests.swift
@@ -16,22 +16,35 @@ import GRPHGenerator
 import GRPHValues
 
 final class DocGenTests: XCTestCase {
-    func testCompleteness() throws {
+    func testDiagnostics() throws {
         print(DocGenerator.builtins.diagnostics.map({ $0.represent() }).joined(separator: "\n"))
         print(DocGenerator.builtins.diagnostics.count, "diagnostics in builtin.grph")
         for diag in DocGenerator.builtins.diagnostics {
             record(XCTIssue(type: .assertionFailure, compactDescription: diag.message, sourceCodeContext: XCTSourceCodeContext(location: XCTSourceCodeLocation(filePath: String(DocGenerator._builtinsSourcePath), lineNumber: diag.token.lineNumber + 1))))
         }
+    }
+    
+    func testSeeAlsoExists() throws {
         for doc in DocGenerator.builtins.documentation.values {
             print(doc.symbol.documentationNames)
             for see in doc.seeAlso {
                 XCTAssertNotNil(DocGenerator.builtins.findDocumentation(sloppyName: see), "\(see) not found in doc for \(doc.symbol.token.literal)")
             }
         }
-        print(DocGenerator.builtins.documentation)
+    }
+    
+    func testFunctionCompleteness() throws {
         for f in NameSpaces.instances.flatMap({ $0.exportedFunctions }) {
             let doc = DocGenerator.builtins.findDocumentation(symbol: SemanticToken(token: Token(lineNumber: 0, lineOffset: f.name.startIndex, literal: f.name[...], tokenType: .function), modifiers: .none, data: .function(f)))
             XCTAssertNotNil(doc, "missing function \(f.signature)")
+        }
+    }
+    
+    func testMethodCompleteness() throws {
+        // this doesn't include methods defined directly in types!
+        for f in NameSpaces.instances.flatMap({ $0.exportedMethods }) {
+            let doc = DocGenerator.builtins.findDocumentation(symbol: SemanticToken(token: Token(lineNumber: 0, lineOffset: f.name.startIndex, literal: f.name[...], tokenType: .method), modifiers: .none, data: .method(f)))
+            XCTAssertNotNil(doc, "missing method \(f.signature)")
         }
     }
 }
