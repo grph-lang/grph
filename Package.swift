a7ae3a7523fe7f8edafbd19ad7a12a4bab69ea07
Snowy <3mille.prenom.nom@gmail.com>
2021-12-28 13:39:34 +0100

    Update dependencies to track upstream instead of now merged forks

--- Package.swift
+++ Package.swift
@@ -33,8 +33,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/Snowy1803/Rainbow", .branch("master")),
-        .package(url: "https://github.com/Snowy1803/sourcekit-lsp", .branch("main")),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
+        .package(url: "https://github.com/apple/sourcekit-lsp", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
